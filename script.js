069832939d05cd7978a93a350d5490f1c8b9bc33
Justin Jeffries <justincyjeffries@gmail.com>
2022-01-02 19:58:43 -0700

    js element 2

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function pburgh(){
     travels.classList.add('lokis');
 })
 }
+
+function disney(){
+    let disney = document.getElementById('#disneyland')
+    disneyland.addEventListener('mouseenter', function(){
+        travels.classList.remove('lokis');
+        travels.classList.add('dl');
+    })
+    disneyland.addEventListener('mouseleave', function(){
+       travels.classList.remove('dl');
+       travels.classList.add('lokis');
+   })
+   }
+   
